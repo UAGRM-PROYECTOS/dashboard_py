5870dc39501434f0979e313626b15319613d1c70
Ali1715 <tacoomaturanoalison@gmail.com>
2024-06-13 16:04:43 -0400

    up settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1b#$-zkpzmyxp*)1-#j@yc#x%7!e-!9n0^jzid73h9#(e!^yn(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['dashboard-py.onrender.com']
 
 
 # Application definition
